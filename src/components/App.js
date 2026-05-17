b756b9897ffd556db717e4c726c0241c2dec2c10
SAHILDWIVEDI1232 <168732444+SAHILDWIVEDI1232@users.noreply.github.com>
2026-05-17 12:41:34 +0530

    Implement tab navigation using useState
    Added state management for tab navigation.

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,32 @@
 
 import React from "react";
+import {useState} from "react"
 import './../styles/App.css';
 
 const App = () => {
+const[tab,setTab]=useState("tab1");
+
+  let content;
+
+  if (tab === "tab1") {
+    content = <h2>This is Tab 1</h2>;
+  } else if (tab === "tab2") {
+    content = <h2>This is Tab 2</h2>;
+  } else {
+    content = <h2>This is Tab 3</h2>;
+  }
+    
+  
   return (
     <div>
         {/* Do not remove the main div */}
+    <ul>
+    <li onClick={()=>setTab("tab1")}>Tab1</li>
+    <li onClick={()=>setTab("tab2")}>Tab2</li>
+    <li onClick={()=>setTab("tab3")}>Tab3</li>
+    
+    </ul>
+  
     </div>
   )
 }
